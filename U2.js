00a6d2e28239c82cc8584d1b9e6579e5d20adbf6
AliveNav <111689692+AliveNav@users.noreply.github.com>
2022-11-25 22:25:42 +0100

    Update U2.js

--- U2.js
+++ U2.js
@@ -1,4 +1,15 @@
+function preventDefault(e){
+  e.preventDefault();
+}
 
+function disableScroll(){
+  document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+function enableScroll(){
+  document.body.removeEventListener('touchmove', preventDefault);
+}
+disableScroll()
+enableScroll()
 // get and set Url values
 let minValue = 0;
 let maxValue = 0;
